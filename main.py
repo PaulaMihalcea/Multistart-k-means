c4f43cba4357ef43c121ccd98acbe0e97c1de408
PaulaMihalcea <mihalceapaula@gmail.com>
2020-02-14 13:38:17 +0100

    Fixed a major error in the main program

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from centroid_distance import centroid_distance
 
 # INIZIALIZZAZIONE (parametri)
 points = 10  # Numero di punti da generare per ogni cluster (righe della matrice)
-features = 2  # Numero di dimensioni per ogni punto (colonne della matrice)
-k = 5
+features = 5  # Numero di dimensioni per ogni punto (colonne della matrice)
+k = 3
 center_range = 5000  # Range di possibili coordinate da assegnare i centri dei cluster
 scale_range = 300  # Range della deviazione standard di ogni cluster
 
@@ -22,11 +22,8 @@ dataset, centri = gen_dataset(points=points, features=features, k=k, center_rang
 # SCELTA CENTROIDI
 centroids = gen_dataset_centroids(dataset, points, k, features)
 
-
 # CALCOLO DISTANZE PUNTI-CENTROIDI
-
-# Calcolo distanze tra punti e centroidi
-dist = centroid_distance(dataset, centroids, k)
+dist = centroid_distance(dataset, centroids, k)  # Calcolo distanze tra punti e centroidi
 
 
 # CLASSIFICAZIONE INIZIALE (individuazione dei cluster)
@@ -43,20 +40,44 @@ for i in range(0, len(dist)):
         labels[i, 0] = i  # Numero del punto
         labels[i, 1] = point_dist[np.argmin(point_dist[:, 1]), 0]  # Numero del cluster a cui appartiene il punto
 
-dataset[2] = labels[:, 1]  # Adesso il dataset contiene una terza colonna indicante il cluster a cui appartiene ogni punto
+dataset[features-1] = labels[:, 1]  # Adesso il dataset contiene una terza colonna indicante il cluster a cui appartiene ogni punto
 
+
+###########################
+
+# LOOP PRINCIPALE
+x = 0  # TODO La regola di arresto va riscritta ('finché i centroidi non vengono più riassegnati')
+punti_nel_cluster = np.zeros(k)  # Numero di punti presenti in ogni cluster
+somma_features = np.zeros(k)  # Somma parziale delle features per ogni cluster
+
+print(dataset)
+#print(dataset.loc[0,2])
+
+while x < 10:
+    for i in range(0, len(dataset)):  # Calcola il centroide per ogni cluster
+        punti_nel_cluster[int(dataset.loc[i, 4])] += 1
+        for j in range(0, features):
+            #somma_features[int(dataset.loc[i, j])] += dataset.loc[i, 2]
+            pass
+
+    x += 1
+    pass
+
+
+###########################
 
 # PLOTTING
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(x=dataset[0], y=dataset[1], c=dataset[2], cmap="tab20b", label='Dataset points', s=15)
+ax.scatter(x=dataset[0], y=dataset[1], c=dataset[features-1], cmap="tab20b", label='Dataset points', s=15)
 ax.scatter(x=centroids[:, 0], y=centroids[:, 1], c='red', marker='x', s=50, label='Centroids')
 #plt.legend(loc='upper left')
 
-plt.show()
+plt.show()  # TODO
 
 
 # TODOs
 
 # TODO Unificare la lingua in commenti e nomi variabili/funzioni (inglese o italiano?)
 # TODO Dare ai centroidi lo stesso colore dei cluster cui si riferiscono (al momento i centroidi sono tutti rossi)
+# TODO Rendere i punti più sparsi/meno raggruppati
