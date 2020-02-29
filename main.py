d995593ce043b392028eb146ef0e80611b044e28
PaulaMihalcea <mihalceapaula@gmail.com>
2020-02-29 10:34:16 +0100

    Separated k-means function from main program

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 from gen_dataset import gen_dataset
-from gen_rand_centroids import gen_rand_centroids
-from label import label
+from k_means import k_means
 
 ##################################################
 
@@ -23,35 +21,9 @@ dataset = gen_dataset(points=points, features=features, k=k, center_range=center
 
 ##################################################
 
-# K-means initialization
+# K-means
 
-centroids = gen_rand_centroids(k=k, features=features, center_range=center_range, scale=scale_high)  # Generates k random centroids
-
-dataset = label(dataset, centroids, k, features)
-
-
-# K-means main loop
-
-points_in_cluster = np.zeros(k)  # Total number of points in each cluster (after classification)
-features_sum = np.zeros((k, features))  # Partial sum of each feature, calculated for each cluster
-
-while True:
-
-    prev_centroids = centroids  # Keeps track of the previous centroids, enabling breaking the loop
-
-    for i in range(0, len(dataset)):  # Computes the centroid for each cluster
-        points_in_cluster[int(dataset.loc[i, features])] += 1  # Counts the number of points in each cluster
-        for j in range(0, features):  # Sums the features of each point, for each cluster
-            features_sum[int(dataset.loc[i, features]), j] += dataset.loc[i, j]
-
-    for i in range(0, k):  # For each cluster, calculates the new centroid
-        for j in range(0, features):
-            centroids[i, j] = features_sum[i, j] / points_in_cluster[i]
-
-    dataset = label(dataset.drop(columns=[features]), centroids, k, features)  # Re-assigns the points in function of the new centroids
-
-    if (centroids == prev_centroids).all():  # Breaks the loop if centroids are not moving anymore
-        break
+dataset, centroids = k_means(dataset, k, features, center_range, scale_high)
 
 ##################################################
 
